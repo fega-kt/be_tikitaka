e47de616fb28a3a1aeca5ef6570535cb268b17a5
Kim thái <fega.kt@gmail.com>
2023-10-14 00:28:36 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -40,7 +40,13 @@ app.use(compression());
 // enable cors
 app.use(cors());
 app.options('*', cors());
-
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
